7e98043f80848a15628c8eb5c2366919da05fe2d
Аружан Туткенова <a.tutkenova@MacBook-Air-Aruzan.local>
2020-02-12 12:07:16 +0600

    ready map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kzmap';
+  clickOne()
+  {
+    console.log("West-Kazakhstan region is click");
+  }
+  clickTwo()
+  {
+    console.log("Atyrau region is click");
+  }
+  clickThree()
+  {
+    console.log("Aktobe region is click");
+  }
+  clickFour()
+  {
+    console.log("Kostanay region is click");
+  }
+  clickFive()
+  {
+    console.log("North-Kazakhstan region kz is click");
+  }
+  clickSix()
+  {
+    console.log("Akmola region is click");
+  }
+  clickSeven()
+  {
+    console.log("Pavlodar region is click");
+  }
+  clickEight()
+  {
+    console.log("Karagandy region is click");
+  }
+  clickNine()
+  {
+    console.log("East-Kazakhstan region is click");
+  }
+  clickTen()
+  {
+    console.log("Almaty region is click");
+  }
+  clickEleven()
+  {
+    console.log("Dzhambul region is click");
+  }
+  clickTwelve()
+  {
+    console.log("Turkistan region is click");
+  }
+  clickThirteen()
+  {
+    console.log("Kyzylorda region is click");
+  }
+  clickFourteen()
+  {
+    console.log("Mangistau region is click");
+  }
 }
